d1626183dace2db0fafcafcc764ac85dfb29b0c8
i32algae <i32algae@uco.es>
2014-12-10 14:09:02 +0100

    cambiando el diagrama.dia

--- proyecto_eclipse/Contacto.h
+++ proyecto_eclipse/Contacto.h
@@ -8,6 +8,18 @@
 #ifndef CONTACTO_H_
 #define CONTACTO_H_
 
+#define CONTACTO_NOMBRE 0
+#define	CONTACTO_APELLIDOS 1
+#define CONTACTO_DNI 2
+#define CONTACTO_TELEFONOS 3
+#define CONTACTO_CORREOE 4
+#define CONTACTO_DIRECCION 5
+#define CONTACTO_ANOTACIONES 6
+#define CONTACTO_FAVORITOS 7
+#define CONTACTO_NCONSULTAS 8
+#define CONTACTO_REDES 9
+
+
 namespace agenda {
 
 struct Cambio{
