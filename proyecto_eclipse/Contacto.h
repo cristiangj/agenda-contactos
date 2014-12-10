c051f32a4273e82e992dcefbbd92ac96fb6c525a
javierbg <i32bagoj@uco.es>
2014-12-10 09:42:41 +0100

    Añadida clase Contacto vacía

---
+++ proyecto_eclipse/Contacto.h
@@ -0,0 +1,20 @@
+/*
+ * Contacto.h
+ *
+ *  Created on: 10/12/2014
+ *      Author: javier
+ */
+
+#ifndef CONTACTO_H_
+#define CONTACTO_H_
+
+namespace agenda {
+
+class Contacto {
+public:
+	Contacto();
+};
+
+} /* namespace agenda */
+
+#endif /* CONTACTO_H_ */
