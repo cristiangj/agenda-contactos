f0bd8e0d169ae26ae4b044407715004de20e9ad8
Aurora <i32estoa@uco.es>
2014-12-26 16:56:20 +0100

    Implementada funcionalidad quitar tildes, arreglada función masUsados y funciones comentadas

--- proyecto_eclipse/GestorAgenda.cpp
+++ proyecto_eclipse/GestorAgenda.cpp
@@ -16,21 +16,26 @@ namespace agenda{
 
 
 #define A_IGUAL_B 0
-#define A_ANTES_QUE_B 1
-#define B_ANTES_QUE_A 2
+#define A_ANTES_B 1
+#define B_ANTES_A 2
 
 
 
 std::string simplificaCadena(std::string cad){
 
-	std::string tildes="ÁáÉéÍíÓóÚú";
-	std::string notildes="AaEeIiOoUu";
-
-	for(int i=0;i<cad.length();i++){
-		for(int j=0 ; j < tildes.size() ; ++j){
-			if(tildes[j] == cad[i]) cad[i] = notildes[j];
-		}
+	//Quita las tildes
+	std::vector <std::string> tildes={"Á","á","É","é","Í","í","Ó","ó","Ú","ú"};	//Al compilar: g++ -std=c++11 archivo.cpp
+	std::vector <std::string> notildes={"A","a","E","e","I","i","O","o","U","u"};
+	size_t reemplazo;
+	for(int i=0;i<tildes.size();i++){
+		while((reemplazo=cad.find(tildes[i])))
+			cad.replace(reemplazo, tildes[i].size(),notildes[i]);
 	}
+
+	//Quita las mayúsculas
+	for(int i=0;i<cad.length();i++)
+		cad[i]=tolower(cad[i]);
+
 	return cad;
 }
 
@@ -44,11 +49,12 @@ int comparaNombres(Contacto a, Contacto b){
 	int i=0;
 	while((i<a.getApellidos().length())&&(i<b.getApellidos().length())){
 		if(a.getApellidos()[i]<b.getApellidos()[i])
-			return A_ANTES_QUE_B;
+			return A_ANTES_B;
 
 		if(a.getApellidos()[i]>b.getApellidos()[i])
-			return B_ANTES_QUE_A;
+			return B_ANTES_A;
 	}
+	//Si llega hasta aquí => apellidos iguales
 
 	a.setNombre(simplificaCadena(a.getNombre()));
 	b.setNombre(simplificaCadena(b.getNombre()));
@@ -56,12 +62,12 @@ int comparaNombres(Contacto a, Contacto b){
 	i=0;
 	while((i<a.getNombre().length())&&(i<b.getNombre().length())){
 			if(a.getNombre()[i]<b.getNombre()[i])
-				return A_ANTES_QUE_B;
+				return A_ANTES_B;
 
 			if(a.getNombre()[i]>b.getNombre()[i])
-				return B_ANTES_QUE_A;
+				return B_ANTES_A;
 	}
-
+	//Si llega hasta aquí => nombres completos iguales
 	return A_IGUAL_B;
 }
 
@@ -71,9 +77,9 @@ int comparaNombres(Contacto a, Contacto b){
 bool GestorAgenda::addContacto(Contacto c){
 
 	for(std::list<Contacto>::iterator i=listaContactos_.begin(); i!=listaContactos_.end(); i++){
-		if(comparaNombres(*i,c)==A_IGUAL_B)
+		if(comparaNombres(*i,c)==A_IGUAL_B)	//Comprobar si altera el formato de los nombre en listaContactos_
 			return false;
-		if(comparaNombres(*i,c)==B_ANTES_QUE_A){
+		if(comparaNombres(*i,c)==B_ANTES_A){
 			listaContactos_.insert(i,c);
 			break;
 		}
@@ -84,14 +90,18 @@ bool GestorAgenda::addContacto(Contacto c){
 
 
 
-void GestorAgenda::borrarContacto(Contacto c){	//Se podría comprobar si se ha borrado un contacto
+bool GestorAgenda::borrarContacto(Contacto c){	//Función cambiada de void a bool
+
+	bool retorna=false;
 
 	for(std::list<Contacto>::iterator i=listaContactos_.begin(); i!=listaContactos_.end(); i++){
-		if(nombresIguales(*i,c)){
+		if(comparaNombres(*i,c)==A_IGUAL_B){	//Comprobar si altera el formato de los nombre en listaContactos_
 			listaContactos_.erase(i);
+			retorna=true;
 			break;
 		}
 	}
+	return retorna;
 }
 
 
@@ -102,6 +112,7 @@ std::list<Contacto> GestorAgenda::buscarContactoApellidos(std::string apellidos)
 	apellidos=simplificaCadena(apellidos);
 	std::list<Contacto> result;
 
+	//Comprobar si altera el formato de los nombre en listaContactos_
 	for(std::list<Contacto>::iterator i=listaContactos_.begin(); i!=listaContactos_.end(); i++){
 		i->getApellidos()=simplificaCadena(i->getApellidos());
 		if(i->getApellidos()==apellidos)
@@ -118,7 +129,7 @@ std::list<Contacto> GestorAgenda::buscarContactoFavoritos(){
 	std::list<Contacto> result;
 
 	for(std::list<Contacto>::iterator i=listaContactos_.begin(); i!=listaContactos_.end(); i++){
-		if(i->getFavorito()==true)
+		if(i->getFavoritos()==true)
 			result.push_back(*i);
 	}
 	return result;
@@ -129,19 +140,13 @@ std::list<Contacto> GestorAgenda::buscarContactoFavoritos(){
 
 std::vector<Contacto> GestorAgenda::masUsados(int cuantos){
 
-	std::vector<Contacto> result(5);
-	Contacto aux,sig;
-	int z;
+	std::vector<Contacto> result(cuantos);
+	std::list<Contacto> aux=listaContactos_;
+	aux.sort(comparaConsultas);
+	auto j=aux.begin();
+	for(int i=0;i<5;i++,j++)
+		result[i]=*j;
 
-	for(std::list<Contacto>::iterator i=listaContactos_.begin(); i!=listaContactos_.end(); i++){
-		for(int j=0;j<cuantos;j++){
-			if(!comparaNConsultas(*i,result[j])){
-				for(z=cuantos;z>0;z--)
-					result[z]=result[z-1];
-				z[0]=*i;
-			}
-		}
-	}
 	return result;
 }
 
--- proyecto_eclipse/GestorAgenda.h
+++ proyecto_eclipse/GestorAgenda.h
@@ -8,58 +8,96 @@
 #ifndef GESTORAGENDA_H_
 #define GESTORAGENDA_H_
 
-//incluir .h de la clase contacto
+//incluir .h de la clase Contacto
+//incluir .h de la clase GestorBackup
+//incluir .h de la clase PersistenciaAgenda
 #include <list>
 #include <string>
 
 namespace agenda {
 
 class GestorAgenda {
+
 private:
+	/*
+	 * Variable tipo list para tener soporte donde modificar los contactos
+	 */
 	std::list<Contacto> listaContactos_;
 
-	inline void cargaListaContactos(std::list<Contacto> nuevaLista) {
-		listaContactos_ = nuevaLista;
-	}
-	;	//Función cambiada de bool a void
+	/*
+	 * Función que copia una lista de contactos a la variable sobre la que vamos a trabajar
+	 * Necesaria para el constructor
+	 */
+	inline void cargaListaContactos(std::list<Contacto> nuevaLista) {listaContactos_ = nuevaLista;};	//Función cambiada de bool a void
 
+	/*
+	 * Función que devuelve distintos enteros según el orden alfabético que tengan los contactos pasados como parámetros
+	 * Necesaria para addContacto y borrarContacto
+	 */
 	int comparaNombres(Contacto a, Contacto b);
-	inline bool comparaNConsultas(Contacto a, Contacto b) {if(a.getNConsultas()>=b.getNConsultas()) return true; else return false;};
 
-std::string simplificaCadena(std::string cad);
+	/*
+	 * Función que compara el número de consultas de dos contactos
+	 * Necesaria para masUsados
+	 */
+	inline bool comparaConsultas(Contacto a, Contacto b) {if(a.getConsultas()>=b.getConsultas()) return true; else return false;};
+
+	/*
+	 * Función que sustituye caracteres críticos para búsqueda de contactos por letras normales
+	 * Necesaria para comparaNombres y buscarContactoApellidos
+	 */
+	std::string simplificaCadena(std::string cad);
+
 
 public:
-GestorAgenda() {
-	// TODO Auto-generated constructor stub
-	PersistenciaAgenda respaldo;
-	std::list<Contacto> nuevaLista = respaldo.leeBD();
-	cargaListaContactos(nuevaLista);
-}
-;
+	GestorAgenda() {
+		// TODO Auto-generated constructor stub
+		PersistenciaAgenda respaldo;	//Por medio de la clase PersistenciaAgenda recupera los datos desde un fichero
+		std::list<Contacto> nuevaLista = respaldo.leeBD();
+		cargaListaContactos(nuevaLista);
+	};
 
-virtual ~GestorAgenda() {
-	// TODO Auto-generated destructor stub
-	PersistenciaAgenda respaldo;
-	respaldo.escribeBD(listaContactos_);
-}
-;
+	virtual ~GestorAgenda() {
+		// TODO Auto-generated destructor stub
+		PersistenciaAgenda respaldo;	//Por medio de la clase PersistenciaAgenda guarda los datos en un fichero
+		respaldo.escribeBD(listaContactos_);
+	};
 
-std::list<Contacto> buscarContactoApellidos(std::string apellidos);
-std::list<Contacto> buscarContactoFavoritos();
-std::list<Contacto> masUsados(int cuantos);
+	/*
+	 * Función que devuelve todos los contactos que coinciden con el apellido pasado como parámetros
+	 */
+	std::list<Contacto> buscarContactoApellidos(std::string apellidos);
 
-bool addContacto(Contacto c);
-inline bool modificarContacto(Contacto c, std::list<Cambio> cambios) {
-	if (c.modificar(cambios))
-		return true;
-	else
-		return false;
-}
-;
-void borrarContacto(Contacto c);
+	/*
+	 * Función que devuelve todos los contactos preseleccionados como favoritos
+	 */
+	std::list<Contacto> buscarContactoFavoritos();
 
-bool imprimirTexto(std::string nombreFichero);
-};
+	/*
+	 * Función que devuelve un número pasado como parámetro de contactos seleccionándolos por el número de consultas de cada contacto
+	 */
+	std::vector<Contacto> masUsados(int cuantos);
 
-} /*namespace agenda*/
+	/*
+	 * Añade un contacto a listaContactos_
+	 */
+	bool addContacto(Contacto c);
+
+	/*
+	 * Llama a la clase Contacto para que aplique modificaciones a un contacto
+	 */
+	inline bool modificarContacto(Contacto c, std::list<Cambio> cambios) {if (c.modificar(cambios)) return true; else return false;};
+
+	/*
+	 * Borra un contacto si está en la lista
+	 */
+	bool borrarContacto(Contacto c);
+
+	/*
+	 * Llama a la clase GestorBackup para que presente los datos actuales de la agenda en un formato legible para el usuario
+	 */
+	bool imprimirTexto(std::string nombreFichero);
+
+}; /* class GestorAgenda */
+} /* namespace agenda */
 #endif /* GESTORAGENDA_H_ */
