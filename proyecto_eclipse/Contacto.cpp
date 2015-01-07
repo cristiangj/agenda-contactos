9cf881eac765c4c75b35b913e08eb637df6cb726
javierbg <i32bagoj@uco.es>
2015-01-07 14:23:33 +0100

    Añadida clase Contacto

--- proyecto_eclipse/Contacto.cpp
+++ proyecto_eclipse/Contacto.cpp
@@ -1,17 +1,160 @@
-/*
- * Contacto.cpp
- *
- *  Created on: 10/12/2014
- *      Author: javier
- */
+#include "contacto.h"
+#include <string>
+#include <vector>
+#include <list>
 
-#include "Contacto.h"
-
-namespace agenda {
-
-Contacto::Contacto() {
-	// TODO Auto-generated constructor stub
+Contacto::Contacto (string nombre, string apellidos, string dni, string correoe, string anotaciones, bool favoritos, int nconsultas) {
+	nombre_=nombre;
+	apellidos_=apellidos;
+	dni_=dni;
+	correoE_=correoe;
+	anotaciones_=anotaciones;
+	favoritos_=favoritos;
+	nconsultas_=nconsultas;
 
 }
 
-} /* namespace agenda */
+
+void Contacto::setNombre (string nombre) {
+	nombre_=nombre;
+}
+
+
+bool Contacto::setApellidos (string apellidos) {
+	if (!apellidos.empty()) {
+		apellidos_=apellidos;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+bool Contacto::setDni (string dni) {
+	int tam=dni.length();
+
+	if (tam==9) {
+		dni_=dni;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+bool Contacto::setCorreoE (string correoe) {
+	int tam=correoe.length();
+	int pos=correoe.find("@");
+	int pos2=correoe.find(".",pos);
+
+	if ( (pos>0) && (pos2>pos+1 && pos2<(tam-1)) ) {
+		correoE_=correoe;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+void Contacto::setAnotaciones (string anotaciones) {
+	anotaciones_=anotaciones;
+}
+
+
+void Contacto::setFavoritos (bool favoritos) {
+	favoritos_=favoritos;
+}
+
+
+void Contacto::setNconsultas (int nconsultas) {
+	nconsultas_=nconsultas;
+}
+void Contacto::anadirNconsultas () {
+	nconsultas_++;
+}
+
+
+bool Contacto::setTelefono (string telefono) {
+	int tam=telefono.length();
+
+	if (tam==9) {
+		telefonos_.push_back(telefono);
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+bool Contacto::setDireccion (Direccion dir) {
+	if ( dir.calle.empty() || (dir.numero<1)) {
+		return false;
+	}
+	else {
+		direcciones_.push_back(dir);
+		return true;
+	}
+}
+
+
+bool Contacto::setRed (CuentaRedSocial red) {
+	redes_.push_back(red);
+	return true;
+}
+
+
+bool Contacto::modificar (list<Cambio> cambios) {
+	list <Cambio> cambios {
+		list <Cambio>::iterator i;
+		for (i=cambios.begin(); i!=cambios.end(); i++) {
+
+			switch(i->idcampo) {
+			case CAMPO_NOMBRE:
+				setNombre(i->valor);
+			break;
+
+			case CAMPO_APELLIDO:
+				setApellidos(i->valor);
+			break;
+
+			case CAMPO_DNI:
+				setDni(i->valor);
+			break;
+
+			case CAMPO_TELEFONO:
+				setTelefono(i->valor);
+			break;
+
+			case CAMPO_CORREOE:
+				setCorreoE(i->valor);
+			break;
+
+			case CAMPO_DIRECCION:
+				setDireccion(i->valor);
+			break;
+
+			case CAMPO_ANOTACION:
+				setAnotaciones(i->valor);
+			break;
+
+			case CAMPO_FAVORITOS:
+				setFavoritos(i->valor);
+			break;
+
+			case CAMPO_CONSULTA:
+				setNconsultas(i->valor);
+			break;
+
+			case CAMPO_RED:
+				setRedes(i->valor);
+			break;
+
+		}
+
+		}
+	}
+}
--- proyecto_eclipse/Contacto.h
+++ proyecto_eclipse/Contacto.h
@@ -1,24 +1,91 @@
-/*
- * Contacto.h
- *
- *  Created on: 10/12/2014
- *      Author: javier
- */
+#ifndef CONTACTO_H
+#define CONTACTO_H
 
-#ifndef CONTACTO_H_
-#define CONTACTO_H_
+#include <iostream>
+#include <string>
+#include <vector>
 
-namespace agenda {
+#define CAMPO_NOMBRE 1
+#define CAMPO_APELLIDO 2
+#define CAMPO_DNI 3
+#define CAMPO_TELEFONO 4
+#define CAMPO_CORREOE 5
+#define CAMPO_DIRECCION 6
+#define CAMPO_ANOTACION	7
+#define CAMPO_FAVORITOS	8
+#define CAMPO_CONSULTA 9
+#define CAMPO_RED 10
 
-struct Cambio{
+using namespace std;
 
+enum tipoRed {twitter, facebook, googleplus};
+
+struct Direccion {
+	string calle;
+	int numero;
+	int piso;
+	int puerta;
+};
+
+struct CuentaRedSocial {
+	tipoRed red;
+	string usuario;
+};
+
+struct Cambio {
+	int idCampo;
+	string valor;
 };
 
 class Contacto {
-public:
-	Contacto();
-};
+	private:
+		string nombre_;
+		string apellidos_;
+		string dni_;
+		vector <string> telefonos_;
+		string correoE_;
+		vector <Direccion> direcciones_;
+		string anotaciones_;
+		bool favoritos_;
+		int nconsultas_;
+		vector <CuentaRedSocial> redes_;
 
-} /* namespace agenda */
+	public:
+		Contacto (string nombre="", string apellidos="", string dni="", string correoe="", string anotaciones="", bool favoritos="", int nconsultas=0);
 
-#endif /* CONTACTO_H_ */
+		void setNombre (string nombre);
+		inline string getNombre () {return nombre_;};
+
+		bool setApellidos (string apellidos);
+		inline string getApellidos () {return apellidos_;};
+		inline string getApellidosyNombre () {return (apellidos_+", "+nombre_);};
+
+		bool setDni (string dni);
+		inline string getDni () {return dni_;};
+
+		bool setCorreoE (string correoe);
+		inline string getCorreoE () {return correoE_;};
+
+		void setAnotaciones (string anotaciones);
+		inline string getAnotaciones () {return anotaciones_;};
+
+		void setFavoritos (bool favoritos);
+		inline bool getFavoritos () {return favoritos_;};
+
+		void setNconsultas (int nconsultas);
+		inline int getNconsultas () {return nconsultas_;};
+		void anadirNconsultas ();
+
+		bool setTelefono (string telefono);
+		inline vector <string> getTelefonos () {return telefonos_;};
+
+		bool setDireccion (Direccion dir);
+		inline vector <Direccion> getDireccion () {return direcciones_;};
+
+		bool setRed (CuentaRedSocial red);
+		inline vector <CuentaRedSocial> getRedes () {return redes_;};
+
+		bool modificar (list<Cambio> cambios);
+}
+
+#endif
